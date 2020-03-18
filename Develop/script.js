29b13190e5cd088281bbbbbc0adbe86201ddf0eb
Nicholas Goudeau <ngoudeau2012@gmail.com>
2020-03-18 16:48:20 -0700

    Established all the variables, prompts, and confirms.

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,29 @@
 // Write a function that takes user input and creates a password
 // based on that input and returns it
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
 var upperCaseLetters = [];
-
-for (i=0; i < letters.length; i++){
-  upperCaseLetters.push(letters[i].toUpperCase());
-  }
-  
+  for (i=0; i < letters.length; i++){
+    upperCaseLetters.push(letters[i].toUpperCase());
+    }
   console.log(upperCaseLetters)
+var specialSymbols = ["!","@","#","$","%","^","&","*","~","?","<",">","+","-","_","="]
+var numbers = ["1","2","3","4","5","6","7","8","9","0"]
+
+var characterLimit = prompt("How many characters long would you like your password to be?")
+var uppercaseCharacters = confirm("Do you want to include uppercase letters in your password?")
+var specialCharacters = confirm("Do you want to include special symbols in your password?")
+var numbersCharacters = confirm("Do you want to include numbers in your password?")
+
+// var passwordCharacters =  letters
+//     if(uppercaseCharacters = "true"){
+//       var passwordCharacters = passwordCharacters.concat(upperCaseLetters);
+//     } else {
+//       var passwordCharacters = passwordCharacters
+//     }
+//     if (specialCharacters = true){
+//       var passwordCharacters = passwordCharacters.concat(specialSymbols);
+//     }
+//     console.log(passwordCharacters);
 
 function generatePassword() {
   /****
