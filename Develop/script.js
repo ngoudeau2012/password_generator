732cf823dbc35c48e424652ad366ee53a43a2b81
Nicholas Goudeau <ngoudeau2012@gmail.com>
2020-03-21 21:45:04 -0700

    Final push with a working password generator.

--- Develop/script.js
+++ Develop/script.js
@@ -1,72 +1,74 @@
 // Write a function that takes user input and creates a password
 // based on that input and returns it
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var upperCaseLetters = [];
-  for (i=0; i < letters.length; i++){
-    upperCaseLetters.push(letters[i].toUpperCase());
-    }
-  console.log(upperCaseLetters)
-var specialSymbols = ["!","@","#","$","%","^","&","*","~","?","<",">","+","-","_","="]
-var numbers = ["1","2","3","4","5","6","7","8","9","0"]
-
-var characterLimit = parseInt(prompt("How many characters long would you like your password to be?"))
-userInput();
-
-function userInput(){
-  if(isNaN(characterLimit) === true){
-    var newCharacterLimit = prompt("Please input a valid number!") 
-    characterLimit = newCharacterLimit;
-    userInput();
-  } else{
-    characterInput();
-  }
-}
-function characterInput(){
-  var uppercaseCharacters = confirm("Do you want to include uppercase letters in your password?");
-  var specialCharacters = confirm("Do you want to include special symbols in your password?");
-  var numbersCharacters = confirm("Do you want to include numbers in your password?");
-  createCharacters(uppercaseCharacters, specialCharacters,numbersCharacters);
-  }
-
-function createCharacters(uppercaseCharacters,specialCharacters,numbersCharacters){
-  var passwordCharacters =  letters
-  if(uppercaseCharacters === true){
-    var passwordCharacters = passwordCharacters.concat(upperCaseLetters);
-  } else {
-    var passwordCharacters = passwordCharacters
-  }
-  if (specialCharacters === true){
-    var passwordCharacters = passwordCharacters.concat(specialSymbols);
-  } else {
-    var passwordCharacters = passwordCharacters
-  }
-  if (numbersCharacters === true){
-    var passwordCharacters = passwordCharacters.concat(numbers);
-  } else {
-    var passwordCharacters = passwordCharacters
-  }
-  console.log(passwordCharacters);
-  randomPassword(passwordCharacters);
-}
-
-  var passwordString ; 
-    
-  function randomPassword(passwordCharacters){
-  for(var i = 0; i < characterLimit; i++){
-    passwordCharacters[Math.floor(Math.random(passwordCharacters.length)* passwordCharacters.length)];
-    passwordString += passwordCharacters[Math.floor(Math.random(passwordCharacters.length)* passwordCharacters.length)];
-    console.log(passwordString);
-  }
-  }
 
 
 function generatePassword() {
   
+  var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var specialSymbols = ["!","@","#","$","%","^","&","*","~","?","<",">","+","-","_","="]
+  var numbers = ["1","2","3","4","5","6","7","8","9","0"]
+  var upperCaseLetters = [];
+    for (i=0; i < letters.length; i++){
+      upperCaseLetters.push(letters[i].toUpperCase());
+      }
+    console.log(upperCaseLetters)
+  var returnPassword = ""
+  var characterLimit = parseInt(prompt("How many characters long would you like your password to be?"))
+  inputError();
+  
+  function inputError(){
+    if(isNaN(characterLimit) === true){
+      var newCharacterLimit = prompt("Please input a valid number!") 
+      characterLimit = newCharacterLimit;
+      inputError();
+    } else{
+      characterInput();
+    }
+  }
+  function characterInput(){
+    var uppercaseCharacters = confirm("Do you want to include uppercase letters in your password?");
+    var specialCharacters = confirm("Do you want to include special symbols in your password?");
+    var numbersCharacters = confirm("Do you want to include numbers in your password?");
+    createCharacters(uppercaseCharacters, specialCharacters,numbersCharacters);
+    }
+  
+  function createCharacters(uppercaseCharacters,specialCharacters,numbersCharacters){
+      var passwordCharacters =  letters
+      if(uppercaseCharacters === true){
+        var passwordCharacters = passwordCharacters.concat(upperCaseLetters);
+      } else {
+        var passwordCharacters = passwordCharacters
+      }
+      if (specialCharacters === true){
+        var passwordCharacters = passwordCharacters.concat(specialSymbols);
+      } else {
+        var passwordCharacters = passwordCharacters
+      }
+      if (numbersCharacters === true){
+        var passwordCharacters = passwordCharacters.concat(numbers);
+      } else {
+        var passwordCharacters = passwordCharacters
+      }
+      console.log(passwordCharacters);
+      var passwordString = passwordCharacters[Math.floor(Math.random(passwordCharacters.length) * passwordCharacters.length)];
+      randomPassword(passwordCharacters,passwordString);
+      
+    } 
+     
+    function randomPassword(passwordCharacters,passwordString){
+    for(var i = 0; i < characterLimit - 1; i++){
+      var newPasswordCharacter = passwordCharacters[Math.floor(Math.random(passwordCharacters.length) * passwordCharacters.length)];
+      passwordString += newPasswordCharacter;
+    }
+    returnPassword = passwordString;
+    console.log(passwordString);
+    
+    }
+  
+    return returnPassword;
 
 
-
-
-  return "sometext";
+  
 }
 
 //////////////////////////////////////////////////////////////
--- README.md
+++ README.md
@@ -5,48 +5,37 @@ Password Generator
 Create a password generator that creates a random password according to the users wants
 
 ## Assumptions
-    - the lowercase alphabet will be used
+- the lowercase alphabet will be used
 
 ## User's choices
-    - How many characters they would like the password to be
-    - Does the user want to include:
-        - capital letters
-        - special characters
-        - numbers
+- How many characters they would like the password to be
+- Does the user want to include:
+    - capital letters
+    - special characters
+    - numbers
 
 ## Pseudo-Code
-    Variables 
-        - Prompt user to get characterLimit variable
-        - Confirm user to get the parameter of the types of characters we will be using
-            - capitalLetters
-            - specialCharacters
-            - numbers
+Variables 
+- Prompt user to get characterLimit variable
+- Confirm user to get the parameter of the types of characters we will be using
+    - capitalLetters
+    - specialCharacters
+    - numbers
     
-    The Character Functions (Created for each characters array)
-        - Only runs when the User selects "true" in the confirm question for each array
-        - Random number Generator for each array being used 
-        - Selects a character from each array
-        - Stores that variable in 
+The Character Functions (Created for each characters array)
+- Only runs when the User selects "true" in the confirm question for each array
+- Random number Generator for each array being used 
+- Selects a character from each array
+- Stores that variable in 
 
-    The Loop
-    - Create a for loop, equal to the length of the amount of characters the user wants
-        - Gather trough a prompt
-    - Create a function that randomly selects from the "randomly selected characters" functions
+The Loop
+- Create a for loop, equal to the length of the amount of characters the user wants
+    - Gather trough a prompt
+- Create a function that randomly selects from the "randomly selected characters" functions
 
 
-    # Other Possible Solutions
+# Other Possible Solutions
+- Create a base variable list that gets the "user approved" variables added it and have the function randomly pick from the list after all variables are added.
 
-    - Create a base variable list that gets the "user approved" variables added it and have the function randomly from the list after all variables are added. \
-
-for loop users char
-random(
-    if (true) randomUpper() U
-    if (true)Randomlower()  h
-    randomSpecial()         
-    randomnumbers()
-)
-
-random
-    basic variables()
-    concat Upper
-    concat Special
+# Problems I encountered
+- The biggest challenge of the home was the proper organization of the code. The code works, but i was having major issues organizing it which made it a hard workspace to operate in. My goal after this homework is submitted is to go through and clean it up. Focus on making each of the functions pure functions, something that takes in an input and gives an output at the end of it.
